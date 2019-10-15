f967078d84a97de9182124723de5c56971764464
Tornblad Mattias <mattias.tornblad@tieto.com>
2019-10-15 18:06:28 +0200

    jhdsfjk

---
+++ Windows/dcpromo.ps1
@@ -0,0 +1,21 @@
+param (
+    [string]$DomainName, 
+    [string]$Password,
+ )
+
+Install-WindowsFeature -Name AD-Domain-Services
+$Params = @{
+    CreateDnsDelegation = $false
+    DatabasePath = 'C:\Windows\NTDS'
+    DomainMode = 'WinThreshold'
+    DomainName = $DomainName
+    DomainNetbiosName = $DomainName.Split('.')[0]
+    ForestMode = 'WinThreshold'
+    InstallDns = $true
+    LogPath = 'C:\Windows\NTDS'
+    NoRebootOnCompletion = $true
+    SafeModeAdministratorPassword = $Password
+    SysvolPath = 'C:\Windows\SYSVOL'
+    Force = $true
+}
+Install-ADDSForest @Params
