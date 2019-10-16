ed769e32c732b4b04dda9b3e3c383662162c47d7
Tornblad Mattias <mattias.tornblad@tieto.com>
2019-10-16 09:43:16 +0200

    sdfklj

---
+++ DSC/NewForest/NewForest.ps1
@@ -0,0 +1,84 @@
+﻿<#PSScriptInfo
+.VERSION 1.0
+.GUID 86c0280c-6b48-4689-815d-5bc0692845a4
+.AUTHOR Microsoft Corporation
+.COMPANYNAME Microsoft Corporation
+.COPYRIGHT (c) Microsoft Corporation. All rights reserved.
+.TAGS DSCConfiguration
+.LICENSEURI https://github.com/PowerShell/ActiveDirectoryDsc/blob/master/LICENSE
+.PROJECTURI https://github.com/PowerShell/ActiveDirectoryDsc
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES
+.PRIVATEDATA
+#>
+
+#Requires -module ActiveDirectoryDsc
+
+<#
+    .DESCRIPTION
+        This configuration will create a new domain with a new forest and a forest
+        functional level of Server 2016.
+#>
+Configuration NewForest
+{
+    param
+    (
+<#
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword,
+#>
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]$Admincreds
+
+
+    )
+
+    Import-DscResource -ModuleName PSDscResources
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS' {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT' {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain 'contoso.com'
+        {
+<#
+            DomainName                    = 'contoso.com'
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+#>
+            DomainName                    = $DomainName
+            Credential                    = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
