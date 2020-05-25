b36edc26bfe1eea7c86820e51ce33c98ffb789af
Mattias Tornblad <mattias.tornblad@tieto.com>
2020-05-25 08:15:55 +0200

    refactor

--- deployDsc.ps1
+++ deployDsc.ps1
@@ -10,10 +10,12 @@ Get-ChildItem -Path $rootPathForFiles -Filter '*.ps1' | ForEach-Object {
     $psd = "$($_.DirectoryName)\$($_.BaseName).psd1"
     if (Test-Path $psd) {
         "Found $psd and include it in DSC configuration"
-        Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force -ConfigurationDataPath $psd
+        # Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force -ConfigurationDataPath $psd
+        Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -force -ConfigurationDataPath $psd -OutputArchivePath "$($_.DirectoryName)\$($_.BaseName).zip"
     } else {
         "No matching configuration data found"
-        Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
+        # Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -ResourceGroupName $resourceGroup -StorageAccountName $storageName -force
+        Publish-AzVMDscConfiguration -ConfigurationPath $ps1 -force -OutputArchivePath "$($_.DirectoryName)\$($_.BaseName).zip"
     }
 }
 
